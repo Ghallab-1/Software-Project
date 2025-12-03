34bb51552c9d27d7d4e404b1f3f97ea0eebc2c7c
Youssef Ghallab <147099496+Ghallab-1@users.noreply.github.com>
2025-12-04 01:22:13 +0200

    Update database.php

--- Database/database.php
+++ Database/database.php
@@ -12,7 +12,7 @@ class Database
            $this->conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname", $this->username, $this->password);
             // set the PDO error mode to exception
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            //echo "Connected successfully";
+            echo "Connected successfully";
           } catch(PDOException $e) {
             echo "Connection failed: " . $e->getMessage();
           }
@@ -20,4 +20,5 @@ class Database
 }
 
 ?>
+
 
