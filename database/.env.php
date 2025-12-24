34ec115432f6af2f5e02347b02ea08569ad4520e
Khaled Muhammad <khaled@example.com>
2025-12-24 04:53:17 +0200

    commit

--- database/.env.php
+++ database/.env.php
@@ -2,5 +2,5 @@
 putenv("DB_HOST=mysql-3f776c4a-khaled-6290.e.aivencloud.com");
 putenv("DB_NAME=attendance_db");
 putenv("DB_USER=avnadmin");
-putenv("DB_PASS=0d4a4515ffc244178634f81cad6fad09");
+putenv("DB_PASS=AVNS_RWEj07KgVFgDc4MF8Yj");
 putenv("DB_PORT=11891");
