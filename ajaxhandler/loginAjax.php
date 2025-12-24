c39ce132c3f165edd4d737b0e2862693f4e3813d
Khaled Muhammad <khaled@example.com>
2025-12-24 04:09:37 +0200

    commit

--- ajaxhandler/loginAjax.php
+++ ajaxhandler/loginAjax.php
@@ -1,13 +1,14 @@
 <?php
 session_start();
 
-/* FORCE ERROR OUTPUT */
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 header("Content-Type: application/json; charset=utf-8");
 
-/* SIMPLE PROOF PHP IS RUNNING */
-echo json_encode(["step" => "file_loaded"]);
+require_once(__DIR__ . "/../database/database.php");
+
+echo json_encode(["step" => "database_included"]);
 exit;
+
