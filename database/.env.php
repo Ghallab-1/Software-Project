779c71d5818f53a961a9240aa053758c34d75dc8
Khaled Muhammad <khaled@example.com>
2025-12-24 02:30:26 +0200

    ,

---
+++ database/.env.php
@@ -0,0 +1,7 @@
+<?php
+// LOCAL ENV — NEVER COMMIT THIS FILE
+putenv("DB_HOST=mysql-3f776c4a-khaled-6290.e.aivencloud.com");
+putenv("DB_NAME=attendance_db");
+putenv("DB_USER=avnadmin");
+putenv("DB_PASSWORD=AVNS_RWEj07KgVFgDc4MF8Yj");
+putenv("DB_PORT=11891");
